2279c4fc6021f62a16ad0385fc61f95d89951a40
nyeinminhtet <nyeinmg904@gmail.com>
2024-07-29 00:32:00 +0630

    descripe-test

--- src/app/__tests__/greet.spec.tsx
+++ src/app/__tests__/greet.spec.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import Greet from "../../components/greet/greet";
 
 describe("Greet", () => {
-  test("render correctly", () => {
+  fit("render correctly", () => {
     render(<Greet />);
     const helloText = screen.getByText(/hello/i);
     expect(helloText).toBeInTheDocument();
@@ -11,7 +11,7 @@ describe("Greet", () => {
 });
 
 describe("Inside", () => {
-  test("render with David", () => {
+  it("render with David", () => {
     render(<Greet name="David" />);
     const text = screen.getByText("Hello David");
     expect(text).toBeInTheDocument();
